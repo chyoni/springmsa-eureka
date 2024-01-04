bb18c02736a419edb71913807f052df3670fccd6
chyonee <chiwon99881@gmail.com>
2024-01-04 14:18:30 +0900

    Eureka server init

---
+++ src/test/java/msa/springmsa_discovery/SpringmsaDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package msa.springmsa_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmsaDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
